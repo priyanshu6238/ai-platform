c9b9165ebb18c5fff0642244fe1587b51e41d7c7
Priyanshu singh <111607560+PriyanSingh@users.noreply.github.com>
2025-04-20 23:08:23 +0530

    Add prestart script and seed data functionality  Introduced a new `prestart.sh` script to automate database initialization tasks including starting the DB, running migrations, and seeding initial data.

--- backend/app/alembic/versions/c43313eca57d_add_document_tables.py
+++ backend/app/alembic/versions/c43313eca57d_add_document_tables.py
@@ -1,7 +1,7 @@
 """Add document tables
 
 Revision ID: c43313eca57d
-Revises: 77dc462dc6b0
+Revises: 0f205e3779ee
 Create Date: 2025-04-07 10:04:10.414165
 
 """
@@ -12,7 +12,7 @@ import sqlmodel.sql.sqltypes
 
 # revision identifiers, used by Alembic.
 revision = "c43313eca57d"
-down_revision = "77dc462dc6b0"
+down_revision = "0f205e3779ee"
 branch_labels = None
 depends_on = None
 
